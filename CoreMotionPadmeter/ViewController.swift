c188546cf035db9242b44bb9169ced06aac83ddf
Jeff Ripke <jripke74@gmail.com>
2017-07-14 16:57:36 -0700

    add padometer logic and displayed on UI

--- CoreMotionPadmeter/ViewController.swift
+++ CoreMotionPadmeter/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    var pedometer = CMPedometer()
+    
     let goGreen = UIColor(red: 0, green: 1.0, blue: 0.15, alpha: 1.0)
     let stopRed = UIColor(red: 1.0, green: 0, blue: 0.15, alpha: 1.0)
     
@@ -17,7 +20,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var stepsLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var paceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stepsLabel.text = "Steps: Not Available"
+    }
+    
     @IBAction func startStopPedometer(_ sender: UIButton) {
-        
+        if sender.titleLabel?.text == "Start" {
+            statusLabel.text = "Pedometer On"
+            sender.setTitle("Stop", for: .normal)
+            sender.backgroundColor = stopRed
+            if CMPedometer.isStepCountingAvailable() {
+                pedometer.startUpdates(from: Date(), withHandler: { (pedometerData, error) in
+                    if let pedometerData = pedometerData {
+                        self.stepsLabel.text = "steps: \(pedometerData.numberOfSteps)"
+                        print("JEFF: \(Date()) -- \(pedometerData.numberOfSteps)")
+                    }
+            })
+            } else {
+                print("Step counting is not available")
+            }
+        } else {
+            pedometer.stopUpdates()
+            statusLabel.text = "Pedometer Off"
+            sender.backgroundColor = goGreen
+            sender.setTitle("Start", for: .normal)
+        }
     }
 }
